a3700872e26fb34cc7aeceb83680b087152ecc14
hashedalgorithm <sanjaykumarcena@gmail.com>
2025-05-22 20:51:06 +0200

    formats and implements types file.py

--- src/eBPF/prod/helper/file.py
+++ src/eBPF/prod/helper/file.py
@@ -1,24 +1,26 @@
 import datetime
+from typing import Optional
 
-class File():
-    perm = ""
-    xxx = ""
-    owner = ""
-    group = ""
-    size = ""
-    created_month = ""
-    created_day = ""
-    created_time = ""
-    name = ""
-    parent = None
-    
-    file_type = ""
+from helper.dir import Dir
 
-    def __init__(self, name, perm="-rwxrwxrwx", xxx="1",
-                 owner="root", group="root", size="4096",
-                 created_month=datetime.datetime.now().strftime("%b"), created_day=datetime.datetime.now().strftime("%d"),
-                 created_time=datetime.datetime.now().strftime("%H:%M"), file_type="", parent=None) -> None:
-        
+
+class File:
+
+    def __init__(
+        self,
+        name: str,
+        perm: str = "-rwxrwxrwx",
+        xxx: str = "1",
+        owner: str = "root",
+        group: str = "root",
+        size: str = 4096,
+        created_month: str = datetime.datetime.now().strftime("%b"),
+        created_day: str = datetime.datetime.now().strftime("%d"),
+        created_time: str = datetime.datetime.now().strftime("%H:%M"),
+        file_type: str = "",
+        parent: Optional[Dir] = None,
+    ) -> None:
+
         self.perm = perm
         self.xxx = xxx
         self.owner = owner
