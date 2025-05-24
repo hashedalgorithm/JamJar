b17404d8ca0351f0af36d1ab994fcfa49ac092ba
hashedalgorithm <sanjaykumarcena@gmail.com>
2025-05-24 11:40:08 +0200

    formats dir_handler.py

--- src/eBPF/prod/dir_handler.py
+++ src/eBPF/prod/dir_handler.py
@@ -2,31 +2,27 @@ from helper.file import File
 from helper.dir import Dir
 
 from helper import helper
+import pprint
+
 
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
 
 
-
-class DIR_handler():
-
-    root = None
-
+class DIR_handler:
 
     def __init__(self) -> None:
         self.root = helper.create_fake_dir_data_helper()
 
-
-
-    def cmd(self, cmd, src_dir=""):
+    def cmd(self, cmd: str, src_dir: str = ""):
         output = None
 
         cmd_name = cmd.split(" ")[0]
@@ -45,27 +41,27 @@ class DIR_handler():
 
         return output
 
-
-
-    def ls(self, args, src_dir):
+    def ls(self, args, src_dir: str):
         output = ""
         target_dir = ""
         src_dir_list = helper.path_to_list_helper(src_dir)
-        
+
         target_dir, args_str = helper.get_main_arg_helper(args)
 
         if "/" in target_dir:
-            target_dir, src_dir_list = helper.target_dir_is_path_helper(target_dir, src_dir_list)
+            target_dir, src_dir_list = helper.target_dir_is_path_helper(
+                target_dir, helper.path_to_list_helper(src_dir_list)
+            )
 
         src_obj = self.root
-        
+
         if src_dir_list != []:
             for layer in src_dir_list:
                 try:
                     src_obj = src_obj.content[layer]
                 except KeyError:
                     return f"ls: cannot access '{src_dir}': No such file or directory"
-        
+
         if target_dir:
             try:
                 target_obj = src_obj.content[target_dir]
@@ -73,10 +69,10 @@ class DIR_handler():
                 return f"ls: cannot access '{target_dir}': No such file or directory"
         else:
             target_obj = src_obj
-        
+
         if target_obj.file_type != "directory":
             output = target_obj.name
-        
+
         else:
             l = "l" in args_str
             a = "a" in args_str
@@ -99,15 +95,19 @@ class DIR_handler():
 
                 if l:
                     output += f"{print_obj.perm} {print_obj.xxx} {print_obj.owner} {print_obj.group} {print_obj.size} {print_obj.created_month} {print_obj.created_day} {print_obj.created_time} "
-                    
-                    if is_dir: output += bcolors.OKBLUE
+
+                    if is_dir:
+                        output += bcolors.OKBLUE
                     output += f"{file}\n"
-                    if is_dir: output += bcolors.ENDC
+                    if is_dir:
+                        output += bcolors.ENDC
 
                 else:
-                    if is_dir: output += bcolors.OKBLUE
+                    if is_dir:
+                        output += bcolors.OKBLUE
                     output += file + " "
-                    if is_dir: output += bcolors.ENDC
+                    if is_dir:
+                        output += bcolors.ENDC
 
             output = output.strip("\n ")
 
@@ -123,19 +123,19 @@ class DIR_handler():
                 output = output.replace(bcolors.ENDC, "")
 
         return output
-    
-
 
     def rm(self, args, src_dir):
         target_file = ""
         src_dir_list = helper.path_to_list_helper(src_dir)
-        
-        target_file, args_str = helper.get_main_arg_helper(args)
-        
-        if "/" in target_file:
-            target_file, src_dir_list = helper.target_dir_is_path_helper(target_file, src_dir_list)
 
-        if not target_file: 
+        target_file, args_str = helper.get_main_arg_helper(args)
+
+        if "/" in target_file:
+            target_file, src_dir_list = helper.target_dir_is_path_helper(
+                target_file, src_dir_list
+            )
+
+        if not target_file:
             output = "rm: missing operand \nTry 'rm --help' for more information."
             return output
 
@@ -144,37 +144,39 @@ class DIR_handler():
         if src_dir_list != []:
             for layer in src_dir_list:
                 src_obj = src_obj.content[layer]
-        
+
         try:
             target_obj = src_obj.content[target_file]
         except KeyError:
-                return f"rm: cannot remove '{target_file}': No such file or directory"
-        
+            return f"rm: cannot remove '{target_file}': No such file or directory"
+
         r = "r" in args_str
         f = "f" in args_str
-        
+
         if not r and target_obj.file_type == "directory":
             return
 
         src_obj.content.pop(target_file)
 
         return None
-    
-
 
     def touch(self, args, src_dir):
         target_file = ""
         path_flag = False
-        
+
         target_file, _ = helper.get_main_arg_helper(args)
 
         if "/" in target_file:
             path_flag = True
-            target_file, src_dir_list = helper.target_dir_is_path_helper(target_file, src_dir)
+            target_file, src_dir_list = helper.target_dir_is_path_helper(
+                target_file, src_dir
+            )
 
-        if not target_file: 
-            output = "touch: missing file operand \nTry 'touch --help' for more information."
-            return output 
+        if not target_file:
+            output = (
+                "touch: missing file operand \nTry 'touch --help' for more information."
+            )
+            return output
 
         file_type_split = target_file.split(".")
 
@@ -185,11 +187,8 @@ class DIR_handler():
 
         target_obj = File(target_file, file_type=file_type)
 
-        helper.add_file_helper(self.root, src_dir_list if path_flag else src_dir, target_obj)
+        helper.add_file_helper(
+            self.root, src_dir_list if path_flag else src_dir, target_obj
+        )
 
         return None
-
-
-
-
-    
