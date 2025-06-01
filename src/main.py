4a70869b36dd89f292a99b3dd67845d748753e05
hashedalgorithm <sanjaykumarcena@gmail.com>
2025-06-01 20:25:41 +0200

    uses bpf 0.1

--- src/main.py
+++ src/main.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     ascii_art()
 
     # Initialize BPF
-    ebpf = EBPF(ebpf_path=os.path.join(os.path.dirname(__file__), "bpf/v0.0.c"))
+    ebpf = EBPF(ebpf_path=os.path.join(os.path.dirname(__file__), "bpf/v0.1.c"))
 
     # Loop with callback to print_event
     ebpf.bpf["events"].open_perf_buffer(ebpf.proc_event)
