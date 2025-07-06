d57c23c9a0893021c4c43d7795eb7db730130ce5
Nivedhidha-I <sinivi177@gmail.com>
2025-07-06 13:09:10 +0200

    handlers changed to handle command calls to individual command files

--- src/handlers/directory_handler.py
+++ src/handlers/directory_handler.py
@@ -1,70 +1,64 @@
 from models.file_system import FileSystem
 from utils.logger import Logger
-from commands.ls import LS
-from utils.parser import Parser
+from utils.parser import CommandParser
+
+from commands.directory.cd import CD
+from commands.directory.cp import CP
+from commands.directory.ls import LS
+from commands.directory.mkdir import MKDIR
+from commands.directory.mv import MV
+from commands.directory.rm import RM
+from commands.directory.rmdir import RMDIR
 
 
 class DirectoryHandler(Logger):
-
     def __init__(self, file_system: FileSystem) -> None:
         super().__init__()
         self.file_system = file_system
+        self.parser = CommandParser()
+        self.command_options_map = {
+            "cd": [],
+            "cp": ["--backup", "--block-size", "-S", "--suffix", "-t", "--target-directory", "--no-preserve", "--preserve", "--reflink", "--sparse", "--update", "--context"],
+            "ls": ["--block-size", "--color", "-F", "--classify", "--format", "--hide", "--hyperlink", "-p", "--indicator-style", "-I", "--ignore", "--quoting-style", "--sort", "--time", "--time-style", "-T", "--tabsize", "-w", "--width"],
+            "mkdir": ["-m", "--mode", "--context"],
+            "mv": ["--backup", "-S", "--suffix", "-t", "--target-directory", "--update"],
+            "rm": ["--interactive"],
+            "rmdir": []
+        }
 
-    def handle(self, command: str, parsed_args: Parser) -> str | None:
-
+    def handle(self, command: str, full_command: str) -> str | None:
+        self.parser.set_options_with_values(self.command_options_map.get(command, []))
+        parsed = self.parser.parse(full_command)
         match command:
             case "cd":
-                return self.cd()
+                cd = CD(self
+                .file_system)
+                return cd.run(parsed)
+
+            case "cp":
+                cp = CP(self.file_system)
+                return cp.run(parsed)
 
             case "ls":
                 ls = LS(self.file_system)
-                return ls.run()
-
-            case "rmdir":
-                return self.rmdir()
+                return ls.run(parsed)
 
             case "mkdir":
-                return self.mkdir()
+                mkdir = MKDIR(self.file_system)
+                return mkdir.run(parsed)
 
             case "mv":
-                return self.mv()
-
-            case "cp":
-                return self.cp()
+                mv = MV(self.file_system)
+                return mv.run(parsed)
 
             case "rm":
-                return self.rm()
+                rm = RM(self.file_system)
+                return rm.run(parsed)
+
+            case "rmdir":
+                rmdir = RMDIR(self.file_system)
+                return rmdir.run(parsed)
 
             case _:
                 print(f"Command '{command}' not recognized by DirectoryHandler.")
                 return None
-
-    def cd(self):
-        # TODO: Implement cd command
-        print("cd not implemented yet")
-        return None
-
-    def rmdir(self):
-        # TODO: Implement rmdir command
-        print("rmdir not implemented yet")
-        return None
-
-    def mkdir(self):
-        # TODO: Implement mkdir command
-        print("mkdir not implemented yet")
-        return None
-
-    def mv(self):
-        # TODO: Implement mv command
-        print("mv not implemented yet")
-        return None
-
-    def cp(self):
-        # TODO: Implement cp command
-        print("cp not implemented yet")
-        return None
-
-    def rm(self):
-        # TODO: Implement rm command
-        print("rm not implemented yet")
-        return None
