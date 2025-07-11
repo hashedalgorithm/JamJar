8bff13293230f895004716d3cf4df926acbb44d6
hashedalgorithm <sanjaykumarcena@gmail.com>
2025-07-11 23:48:19 +0200

    updates types for file_system_entry_properties

--- src/models/file_system_entry_properties.py
+++ src/models/file_system_entry_properties.py
@@ -11,18 +11,31 @@ class FileSystemEntryProperties:
         owner: str = "root",
         group: str = "root",
         size: str = 4096,
+        extension: str = "",
+        parent: str = "",
         created_month: str = datetime.datetime.now().strftime("%b"),
         created_day: str = datetime.datetime.now().strftime("%d"),
         created_time: str = datetime.datetime.now().strftime("%H:%M"),
     ) -> None:
 
-        self.perm = perm
-        self.xxx = xxx
-        self.owner = owner
-        self.group = group
-        self.size = size
-        self.created_month = created_month
-        self.created_day = created_day
-        self.created_time = created_time
-        self.name = name
-        self.parent = None
+        self.perm: str = perm
+        self.xxx: str = xxx
+        self.owner: str = owner
+        self.group: str = group
+        self.size: str = size
+        self.created_month: str = created_month
+        self.created_day: str = created_day
+        self.created_time: str = created_time
+        self.name: str = name
+        self.extension: str = extension
+        self.parent: str = parent
+        self.path: str = f"{name}{"." if extension else ""}{extension}"
+
+    def __repr__(self) -> str:
+        return (
+            f"FileSystemEntryProperties("
+            f"name={self.name!r},{self.extension!r}, perm={self.perm!r}, xxx={self.xxx!r}, "
+            f"owner={self.owner!r}, group={self.group!r}, size={self.size!r}, "
+            f"created_month={self.created_month!r}, created_day={self.created_day!r}, "
+            f"created_time={self.created_time!r}, parent={self.parent!r})"
+        )
