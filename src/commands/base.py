634893cd70f57cdc833506c9af3dcb6eba770e0c
hashedalgorithm <sanjaykumarcena@gmail.com>
2025-07-11 23:43:07 +0200

    updates CommandBase class

--- src/commands/base.py
+++ src/commands/base.py
@@ -3,8 +3,9 @@ class CommandBase:
     Base class for all commands.
     """
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, version: str = "v0"):
         self.name = name
+        self.version: str = version
 
     def run(self, *args, **kwargs):
         """
@@ -12,20 +13,12 @@ class CommandBase:
         """
         raise NotImplementedError("Subclasses must implement this method.")
 
-    def extract_flags(self, args: list[str]) -> list[str]:
+    def help(self) -> list[str]:
         """
-        Extract flags from the command arguments.
+        Return help readme
         """
-        return [arg for arg in args if arg.startswith("-")]
+        raise NotImplementedError("Subclasses must implement this method")
 
-    def extract_paths(self, args: list[str]) -> list[str]:
-        """
-        Extract paths from the command arguments.
-        """
-        return [arg for arg in args if not arg.startswith("-")]
-
-    def initialize_flag_map(self, flags: list[str]) -> dict:
-        """
-        Initialize the flag map based on the provided flags.
-        """
-        raise NotImplementedError("Subclasses must implement this method.")
+    def version(self) -> list[str]:
+        """Return version readme"""
+        raise NotImplementedError("Subclasses must implement this method")
